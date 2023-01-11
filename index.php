d67942661c1d9bce4ebc5789d1fc6f1c65aa0a17
Sumit Sethia <sumitjainsj1997@gmail.com>
2023-01-11 15:16:36 +0530

    updation

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
 // require_once "helper/env.php";
 // require_once "helper/request.php";
-<<<<<<< HEAD
-
 array_map(fn($f)=> include $f, glob("helper/*.php"));
-=======
-// Gyanu 
 array_map(function($filename){
     require_once $filename;
 }, glob("helper/*.php"));
->>>>>>> c2a22b21ff8a631406bee5389ce3c88a7c683295
 // print_r(glob("helper/*.php"));
 spl_autoload_register(function($classname){
 $path="apps/libs/$classname.php";
